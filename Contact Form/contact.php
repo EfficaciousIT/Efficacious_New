292ebcdcca93645faf26c335d93881ec143b1ed7
Shubham4099 <shubham.efficacious@gmail.com>
2022-05-21 12:51:37 +0530

    Change php and contact page

--- Contact Form/contact.php
+++ Contact Form/contact.php
@@ -1,42 +1,24 @@
 <?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$address= $_POST['address'];
+$number= $_POST['number'];
+$message= $_POST['subject'];
+$msg_subject= $_POST['msg_subject'];
+$to = "mailto:shubham.efficacious@gmail.com";
+$subject = "e-smartrestaurant Contact Form";
+$txt ="Name = ". $name . "\r\n Email = " . $email ."\r\n  
+Address = " . $address . " \r\n   Phone Number = " . $number . " \r\n 
+subject =" . $message;
+$headers = "From: " $email;
 
-$result = "";
-   if(isset($_POST['submit']))
-   {
-       
-       include("class.phpmailer.php"); 
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+    print "<p>Success</p>";
+}
+else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
 
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers.= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers.= "From: EFFICACIOUS  <info@efficacious.co.in>" . "\r\n"; 
-
-   	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>EFFICACIOUS</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href=''><img src='../img/logo_pic.png' alt='EFFICACIOUS'></a><br><br>";
-	$body .= "</td></tr></thead> <tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$_POST['name']}</td> ";
-	$body .= "</tr>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$_POST['email']}</td>";
-	$body .= "</tr>";
-	$body .= "<td style='border:none;'><strong>Subject:</strong> {$_POST['subject']}</td>";
-	$body .= "</tr>";
-	$body .= "<td style='border:none;'><strong>Phone Number:</strong> {$_POST['phone Number']}</td>";
-	$body .= "</tr>";
-	$body .= "<td style='border:none;'><strong>Address :</strong> {$_POST['Address']}</td>";
-	
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'> <strong>Message :</strong> {$_POST['message']}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-
-	$msg = $body;
-	$msg = wordwrap($msg,70);
-	$email=$_POST['email'];
-	mail("info@efficacious.co.in","EFFICACIOUS",$msg,$headers);
-
-   	}
-   header("location : index.html");
 ?>
-
